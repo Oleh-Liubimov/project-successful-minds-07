df75e8ae1b0cf02c31d6b4c8ad21f253d9d81992
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-09-11 15:57:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project-successful-minds-07/',
-  build: {
-    sourcemap: true,
-    minify: 'esbuild', // Используйте встроенную минификацию
-  },
 });
